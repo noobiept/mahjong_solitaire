378c46a24cabe2d1766667b7b5f8dc8b6c9296f7
Pedro Ferreira <plferreira4@gmail.com>
2017-04-12 04:30:07 +0100

    - update the map editor   - update to python v3.6   - update to django v1.11   - simplify the settings code   - move the static/template files to its own directory

--- map_editor/map_editor/urls.py
+++ map_editor/map_editor/urls.py
@@ -1,22 +1,9 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
+import views
 
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
 
-urlpatterns = patterns('',
-    # Examples:
-    url(r'^$', 'views.home', name='home'),
-
-    url(r'^save_map/?$', 'views.saveMap', name='saveMap'),
-
-    url(r'^load_map/?$', 'views.loadMap', name='loadMap'),
-
-    # url(r'^map_editor/', include('map_editor.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
-)
+urlpatterns = [
+    url(r'^$', views.home, name='home'),
+    url(r'^save_map/?$', views.saveMap, name='saveMap'),
+    url(r'^load_map/?$', views.loadMap, name='loadMap'),
+]
