b09efad61113347fec4b4e6b5431291d3fdd365d
Pedro Ferreira <plferreira4@gmail.com>
2019-01-31 23:09:33 +0000

    Remove the 'chrome' related storage code.
    - Also remove the 'AppStorage.removeData()' (not being used).

--- scripts/app_storage.ts
+++ scripts/app_storage.ts
@@ -1,9 +1,15 @@
+export interface Dict
+    {
+    [ key: string ]: Object
+    }
+
+
 /**
  * Calls the `callback` with a dictionary that has all the requested keys/values from `localStorage`.
  */
-function get_localStorage( keys, callback )
+export function getData( keys: string[], callback: (objects: Dict) => void )
     {
-    var objects = {};
+    var objects: Dict = {};
 
     for (var a = 0 ; a < keys.length ; a++)
         {
@@ -21,7 +27,7 @@ function get_localStorage( keys, callback )
  * Sets the given key/value into `localStorage`. Calls the `callback` when its done.
  * Converts the value to string (with json).
  */
-function set_localStorage( items, callback )
+export function setData( items: Dict, callback: () => void )
     {
     for ( var key in items )
         {
@@ -34,104 +40,5 @@ function set_localStorage( items, callback )
     if ( callback )
         {
         callback();
-        }
-    }
-
-
-/**
- * Remove the given keys from `localStorage`.
- */
-function remove_localStorage( items, callback )
-    {
-    for ( var key in items )
-        {
-        if ( items.hasOwnProperty( key ) )
-            {
-            localStorage.removeItem( key );
-            }
-        }
-
-    if ( callback )
-        {
-        callback();
-        }
-    }
-
-
-/**
- * Calls the `callback` with a dictionary that has all the requested keys/values from `chrome.storage.local`.
- */
-function get_chromeStorage( keys, callback )
-    {
-    chrome.storage.local.get( keys, callback );
-    }
-
-
-/**
- * Sets the given key/value into `chrome.storage.local`. Calls the `callback` when its done.
- * Converts the value to string (with json).
- */
-function set_chromeStorage( items, callback )
-    {
-    chrome.storage.local.set( items, callback );
-    }
-
-
-/**
- * Remove the given keys from `chrome.storage.local`.
- */
-function remove_chromeStorage( items, callback )
-    {
-    chrome.storage.local.remove( items, callback );
-    }
-
-
-/**
- * Uses the `chrome storage` if it's available (when running as a chrome app), otherwise uses the `localStorage`.
- */
-export function getData( keys, callback )
-    {
-    if ( window.chrome && window.chrome.storage )
-        {
-        get_chromeStorage( keys, callback );
-        }
-
-    else
-        {
-        get_localStorage( keys, callback );
-        }
-    }
-
-
-/**
- * Uses the `chrome storage` if it's available (when running as a chrome app), otherwise uses the `localStorage`.
- */
-export function setData( items, callback )
-    {
-    if ( window.chrome && window.chrome.storage )
-        {
-        set_chromeStorage( items, callback );
-        }
-
-    else
-        {
-        set_localStorage( items, callback );
-        }
-    }
-
-
-/**
- * Uses the `chrome storage` if it's available (when running as a chrome app), otherwise uses the `localStorage`.
- */
-export function removeData( items, callback )
-    {
-    if ( window.chrome && window.chrome.storage )
-        {
-        remove_chromeStorage( items, callback );
-        }
-
-    else
-        {
-        remove_localStorage( items, callback );
         }
     }
