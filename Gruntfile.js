12a697f3a7777e4fedd317ba583ac469885a821d
Pedro Ferreira <plferreira4@gmail.com>
2019-02-09 16:55:46 +0000

    Fix, when updating the libraries need to clean it first before copying the libraries.

--- Gruntfile.js
+++ Gruntfile.js
@@ -18,6 +18,7 @@ module.exports = function(grunt) {
         // delete the destination folder and the previously compiled javascript files
         clean: {
             release: [DEST, Path.join(ROOT, "scripts/**/*.js")],
+            libraries: [Path.join(ROOT, "libraries/**")],
         },
 
         // copy the audio and libraries files
@@ -34,12 +35,6 @@ module.exports = function(grunt) {
                         expand: true,
                         cwd: Path.join(ROOT, "node_modules/preloadjs/lib/"),
                         src: "preloadjs.min.js",
-                        dest: Path.join(ROOT, "libraries/"),
-                    },
-                    {
-                        expand: true,
-                        cwd: Path.join(ROOT, "node_modules/soundjs/lib/"),
-                        src: "soundjs.min.js",
                         dest: Path.join(ROOT, "libraries/"),
                     },
                 ],
@@ -118,6 +113,10 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks("grunt-contrib-clean");
 
     // tasks
+    grunt.registerTask("update_libraries", [
+        "clean:libraries",
+        "copy:libraries",
+    ]);
     grunt.registerTask("default", [
         "clean",
         "typescript",
